05c66c70424496a7fc5343223266a54b2e67bd1d
idgm5 <idgm@pm.me>
2020-07-17 16:36:38 -0500

    Fix App.js now it renders all buttons as required

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-ReactDOM.render(
-  <div id="container">
-    <Display />
-    <ButtonPanel />
-  </div>,
-  document.getElementById('root')
-);
+function App() {
+  return (
+    <div id="container">
+      <Display />
+      <ButtonPanel />
+    </div>
+  );
+}
+
+export default App;
