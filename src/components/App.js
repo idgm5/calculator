671045290724b8fffc5fc7a2aaca51155bcef9ce
idgm5 <idgm@pm.me>
2020-07-20 18:20:47 -0500

    import calculate in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
+/* eslint-disable  no-unused-vars */
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import Calculate from '../logic/calculate';
 
 function App() {
   return (
