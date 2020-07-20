fea194469bd95aaf7d30f7629c6db87776edf6f3
idgm5 <idgm@pm.me>
2020-07-20 12:29:43 -0500

    Add logic to operate using Big.js methods

---
+++ src/logic/operate.js
@@ -0,0 +1,28 @@
+import Big from 'big.js';
+
+const Operate = (numberOne, numberTwo, operation) => {
+  const one = Big(numberOne);
+  const two = Big(numberTwo);
+
+  if (operation === '+') {
+    return one.plus(two);
+  }
+
+  if (operation === '-') {
+    return one.minus(two);
+  }
+
+  if (operation === 'X') {
+    return one.multiply(two);
+  }
+
+  if (operation === '/') {
+    return one.div(two);
+  }
+
+  if (operation === '%') {
+    return one.mod(two);
+  }
+};
+
+export default Operate;
